d0f8d56a27430d0d4436f3334a6088c483c9cf5b
John Rees <john@bitsushi.com>
2020-06-24 20:51:25 +0100

    update convert

--- src/convert.sh
+++ src/convert.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+sudo apt install gdal-bin -y;
 mkdir geojsons;
 cd gmls;
-for f in *.gml; do ogr2ogr -f "GeoJSON" "geojsons/${f%.gml}.geojson" ${f} -s_srs EPSG:27700 -t_srs EPSG:4326; done
+wget https://raw.githubusercontent.com/johnrees/Land-Registry-INSPIRE-Index-Polygons-tools/master/Land_Registry_Cadastral_Parcels.gfs;
+export GML_GFS_TEMPLATE=Land_Registry_Cadastral_Parcels.gfs;
+for f in *.gml; do ogr2ogr -f "GeoJSON" "../geojsons/${f%.gml}.geojson" ${f} -s_srs EPSG:27700 -t_srs EPSG:4326; done
 cd ..;
