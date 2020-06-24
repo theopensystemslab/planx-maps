f12b568990093958404ea0f98bff24822abe5d20
John Rees <john@bitsushi.com>
2020-06-24 19:44:02 +0100

    update scripts

--- src/convert.sh
+++ src/convert.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir geojsons;
-for f in gmls/*.gml; do ogr2ogr -f "GeoJSON" "geojsons/${f%.gml}.geojson" ${f} -s_srs EPSG:27700 -t_srs EPSG:4326; done
+cd gmls;
+for f in *.gml; do ogr2ogr -f "GeoJSON" "geojsons/${f%.gml}.geojson" ${f} -s_srs EPSG:27700 -t_srs EPSG:4326; done
+cd ..;
--- src/extract.sh
+++ src/extract.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir gmls;
-for f in zips/*.zip; do unzip -p "$f" Land_Registry_Cadastral_Parcels.gml > "gmls/${f%.zip}.gml"; done
+cd zips;
+for f in *.zip; do unzip -p "$f" Land_Registry_Cadastral_Parcels.gml > "../gmls/${f%.zip}.gml"; done
+cd ..;
